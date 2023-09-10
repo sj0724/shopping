c02c7c7f1dac729bc91940380050f95058825468
suminoh <osumin740@gmail.com>
2023-09-10 23:07:30 +0900

    pr 용 커밋

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,52 +1,60 @@
 import React from "react";
-import { useState,useEffect} from "react";
+import { useState, useEffect } from "react";
 import BagList from "../components/BagList";
 
 const Cart = () => {
-    const [shop,setShop] = useState([]);
-    const [price,setPrice] = useState(0)
+  const [shop, setShop] = useState([]);
+  const [price, setPrice] = useState(0);
 
-    useEffect(()=>{
-        const shopList = localStorage.getItem('bag');
-        const bagPrice = localStorage.getItem('price');
-        if(shopList)setShop(JSON.parse(shopList))
-        if(bagPrice)setPrice(JSON.parse(bagPrice))
-    },[])
+  useEffect(() => {
+    const shopList = localStorage.getItem("bag");
+    //price 는 로컬스토리지로 저장하지 않아도 될듯
+    const bagPrice = localStorage.getItem("price");
+    if (shopList) setShop(JSON.parse(shopList));
+    if (bagPrice) setPrice(JSON.parse(bagPrice));
+  }, []);
 
-    function up(e){
-        const upPrice = price + e.price
-        setPrice(upPrice)
+  //up, down 함수도 없애기
+  function up(e) {
+    const upPrice = price + e.price;
+    setPrice(upPrice);
+  }
 
-    }
-    
-    function down(e){
-        const downPrice = price - e.price
-        setPrice(downPrice)
-    }
+  function down(e) {
+    const downPrice = price - e.price;
+    setPrice(downPrice);
+  }
 
-    function delate(e){
-        const delateList = shop.filter((item)=>item.item_no !== e.item_no)
-        const delatePrice = price - e.price
-        setShop(delateList)
-        setPrice(delatePrice)
-        localStorage.setItem('bag',JSON.stringify(delateList))
-        localStorage.setItem('price',JSON.stringify(delatePrice))
-    }
+  function delate(e) {
+    const delateList = shop.filter((item) => item.item_no !== e.item_no);
+    const delatePrice = price - e.price;
+    setShop(delateList);
+    setPrice(delatePrice);
+    localStorage.setItem("bag", JSON.stringify(delateList));
+    localStorage.setItem("price", JSON.stringify(delatePrice));
+  }
 
-    return(
-        <>
-        <div className="alertBag">
-            <h1>장바구니가 비어있습니다!</h1>
-        </div>
-         <div className="cart">
-             {shop.map((item,index)=>
-             (<BagList item={item} key={index} up={up} down={down} delate={delate}/>))}
-        </div>
-        <div className="total_modal">
-            <h1>총 합계: {price}</h1>
-        </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="alertBag">
+        <h1>장바구니가 비어있습니다!</h1>
+      </div>
+      <div className="cart">
+        {shop.map((item, index) => (
+          <BagList
+            item={item}
+            key={index}
+            up={up}
+            down={down}
+            delate={delate}
+          />
+        ))}
+      </div>
+      <div className="total_modal">
+        <h1>총 합계: {price}</h1>
+      </div>
+    </>
+  );
+};
 
 export default Cart;
--- src/pages/Product.js
+++ src/pages/Product.js
@@ -1,56 +1,83 @@
-import React from "react"
-import productItems from "../components/productItems"
-import Merchandise from "../components/Merchandise"
-import { useState,useEffect } from "react"
-import addBtn from "../components/addBtn"
+import React from "react";
 
-const Product = () =>{
-    const [list,setList] = useState(productItems)
-    const [bag,setBag] = useState([])
-    const [price,setPrice] = useState(0)
+//더미 데이터(mock data)에 대해서 공부해보기, json 파일로 관리
+//폴더 구조도 같이 생각해보면 좋을 듯
+import productItems from "../components/productItems";
+import Merchandise from "../components/Merchandise";
+import { useState, useEffect } from "react";
+import addBtn from "../components/addBtn";
 
-    useEffect(()=>{
-        const localbag = localStorage.getItem('bag')
-        if(localbag)setBag(JSON.parse(localbag))
-    },[])
+const Product = () => {
+  const [list, setList] = useState(productItems);
 
-    const increaseBag = (item) => {
-        if(test(item) === item.item_name){
-            alert('이미 같은 상품이 장바구니에 담겨있습니다!')
-            return false;
-        }else if(bag.length < 3){
-            let bagList = [...bag,item]
-            setBag(bagList)
-            localStorage.setItem('bag',JSON.stringify(bagList))
-            let totalPrice = price + item.price
-            setPrice(totalPrice)
-            localStorage.setItem('price',JSON.stringify(totalPrice))
-            alert('장바구니에 상품이 담겼습니다!')
-            return;
-        }else{
-            alert('장바구니에는 최대 3개의 물건만 남을수 있습니다!')
-        }
+  //bag의 초기값을 productItems로 설정, 새로운 필드 추가 ex)count 로 해결해보기
+  const [bag, setBag] = useState([]);
+
+  //bag 수정하면 price 변수 필요 없을 듯
+  const [price, setPrice] = useState(0);
+
+  useEffect(() => {
+    const localbag = localStorage.getItem("bag");
+    if (localbag) setBag(JSON.parse(localbag));
+  }, []);
+
+  //이 함수도 bag 수정 후 Merchandise 으로 옮겨도 될듯(확실x)
+  //해당 컴포넌트에서만 사용하는 함수는 그 안에 있는게 가독성 측면에서 더 좋음
+  const increaseBag = (item) => {
+    if (test(item) === item.item_name) {
+      //alert return 값에 대해 검색해보기
+      alert("이미 같은 상품이 장바구니에 담겨있습니다!");
+      return false;
+    } else if (bag.length < 3) {
+      //let 과 count 의 차이점에 대해 알고 있는지
+      //let 을 사용한 이유에 대해 생각해보기
+      let bagList = [...bag, item];
+      setBag(bagList);
+      localStorage.setItem("bag", JSON.stringify(bagList));
+      let totalPrice = price + item.price;
+      setPrice(totalPrice);
+      localStorage.setItem("price", JSON.stringify(totalPrice));
+      alert("장바구니에 상품이 담겼습니다!");
+      return;
+    } else {
+      alert("장바구니에는 최대 3개의 물건만 남을수 있습니다!");
+    }
     return;
+  };
+
+  //함수명 수정 필요
+  //그리고 위에 bag 수정하면 이 함수도 수정 필요할듯
+  const test = (e) => {
+    for (let i = 0; i < bag.length; i++) {
+      if (bag[i] === e) {
+        return e.item_name;
+      }
     }
+    return null;
+  };
 
-    const test = (e) => {
-        for(let i = 0; i < bag.length; i++){
-             if(bag[i] === e){
-                return e.item_name;
-            }
-        }
-        return null;
-    }
+  /**
+   * list 가 빈값일 때 고려해서 수정해보기
+   * 아래 코드로는 렌더링 이슈 발생할 수 있음
+   * 추가로 공부해보면 좋은것들
+   * csr, ssr
+   * 조건부 렌더링
+   * **/
 
-    return(
-        <>
-         <div className="Board">
-            {list.map((item, index)=> (
-            <Merchandise item={item} key={index} increaseBag={increaseBag} bag={bag}/>
-            ))}
-         </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="Board">
+        {list.map((item, index) => (
+          <Merchandise
+            item={item}
+            key={index}
+            increaseBag={increaseBag}
+            bag={bag}
+          />
+        ))}
+      </div>
+    </>
+  );
+};
 
-export default Product
+export default Product;
