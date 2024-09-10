a02cb3c687093d18f79a19cf4f9038938141eb25
sj0724 <sj07245@naver.com>
2024-09-10 22:26:03 +0900

    fix: 불필요한 전역 변수 삭제

--- src/states/productState.js
+++ src/states/productState.js
@@ -1,6 +1,5 @@
 import { atom } from 'recoil';
 import { recoilPersist } from 'recoil-persist';
-import mockData from '../mockData.json';
 
 const { persistAtom } = recoilPersist();
 
@@ -8,9 +7,4 @@ export const productState = atom({
   key: 'productState',
   default: [],
   effects_UNSTABLE: [persistAtom],
-});
-
-export const productList = atom({
-  key: 'productList',
-  default: [...mockData],
 });
